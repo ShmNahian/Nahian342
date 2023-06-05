5fa1aa30257b3e302c2007d568b14e5b9a6d5ffc
Shamsul huda Md Nahian <114670807+ShmNahian@users.noreply.github.com>
2023-06-05 10:36:38 +0600

    Add files via upload

---
+++ nn.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+void calculatepower(double base,double exp)
+{
+    double result=1,i;
+
+    for(i=1;i<=exp;i++)
+    {
+        result = result * base;
+    }
+        printf("%.1lf\n",result);
+
+}
+
+
+
+
+int main()
+{
+
+    double base,exp;
+
+    printf("Enter base : ");
+    scanf("%lf",&base);
+    printf("Enter exp : ");
+    scanf("%lf",&exp);
+
+    calculatepower(base,exp);
+}

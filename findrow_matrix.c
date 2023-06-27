cd99dc12e35fb8c33c46df07578659d13b33044c
Shamsul huda Md Nahian <114670807+ShmNahian@users.noreply.github.com>
2023-06-27 13:08:17 +0600

    Update findrow_matrix.c

--- findrow_matrix.c
+++ findrow_matrix.c
@@ -19,4 +19,35 @@ int main()
         printf("\n");
     }
     return 0;
+}
 
+/* sum
+#include<stdio.h>
+int main()
+{
+    int arr[100][100];
+    int r,c,x,i,j;
+    int sum=0;
+    scanf("%d%d",&r,&c);
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+        scanf("%d",&arr[i][j]);
+        }
+    }
+    scanf("%d",&x);
+    for(i=0;i<r;i++)
+    {
+
+
+        printf("%d ",arr[i][x-2]);
+        printf("\n");
+        sum+=arr[i][x-2];
+
+    }
+    printf("%d ",sum);
+
+    return 0;
+}
+*/
